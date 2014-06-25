775a6aeaa76ed3116f6cccd3d543a03b1399d385
Sebastian Schaffert <sschaffert@apache.org>
2014-06-25 20:46:42 +0200

    code documentation

--- relatedness/relatedness_shortest_path.h
+++ relatedness/relatedness_shortest_path.h
@@ -11,25 +11,39 @@ extern "C" {
 namespace mico {
 
   namespace relatedness {
+
     /**
-     * An implementation of relatedness using shortest path computation (up to a maximum number of
-     * edges) over the knowledge graph.
+     * An implementation of relatedness using shortest path computation (Dijkstra) up to a maximum number of
+     * edges over the knowledge graph.
      */
     class shortest_path : public virtual base {
 
       int max_dist;
 
       // helper structures (not thread safe!)
-      pqueue_t queue;
+      pqueue_t queue; // priority queue used by the Dijkstra algorithm
 
-      double* dist;
-      int*    idx;
+      double* dist;   // vector with current distance used by Dijkstra algorithm
+      int*    idx;    // reverse lookup index pointing from vertice ids to queue positions
 
     public:
-
+      
+      /**
+       * Initialise a shortest path computation over the given graph up to the given maximum distance.
+       */
       shortest_path(rgraph* graph, int max_dist);
+
+      /**
+       * Cleanup helper structures
+       */
       ~shortest_path();
 
+      /**
+       * Relatedness computation via shortest path computation in the underlying graph up to a maximum
+       * distance for improved performance. Since it will be called very often, this method is heavily
+       * optimized for the internal igraph data structures. It uses shared instance data structures, so
+       * calling this method on the same instance in multiple threads is not safe.
+       */
       double relatedness(const char* from, const char* to);
 
 
